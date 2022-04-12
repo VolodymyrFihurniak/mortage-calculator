c80f0ff7d21d4078d49b5133c3dd487e25298fa0
Volodymyr Fihurniak <volodymyr.fihurniak@gmail.com>
2022-04-12 15:45:06 +0300

    Added UI components

---
+++ src/components/ui/BaseTooltip.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="relative">
+    <div class="h-full" @mouseenter="toggle" @mouseleave="toggle">
+      <slot />
+    </div>
+    <div v-show="isShown" :class="classes"><span v-html="text"></span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String
+  },
+  data () {
+    return {
+      isShown: false
+    }
+  },
+  computed: {
+    classes () {
+      return [
+        'bg-gray-600',
+        'bg-opacity-80',
+        'rounded-sm',
+        'text-white',
+        'text-xs',
+        'whitespace-nowrap',
+        'p-2',
+        'absolute',
+        'margin-top-auto',
+      ]
+    }
+  },
+  methods: {
+    toggle () {
+        this.isShown = !this.isShown
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.bg-gray-600 {
+  background-color: #6c757d !important;
+}
+
+.bg-opacity-80 {
+  background-color: rgba(108, 117, 125, 0.8) !important;
+}
+
+.rounded-sm {
+    border-radius: 0.3rem !important;
+}
+
+.text-white {
+    color: #fff !important;
+}
+
+.text-xs {
+    font-size: 0.75rem !important;
+}
+
+.whitespace-nowrap {
+    white-space: nowrap !important;
+}
+
+.p-2 {
+    padding: 0.5rem !important;
+}
+
+.absolute {
+    position: absolute !important;
+}
+
+.margin-top-auto {
+    margin-top: auto !important;
+}
+
+</style>
---
+++ src/components/ui/MyButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <button class="btn">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'my-button'
+}
+</script>
+
+<style>
+.btn {
+    background: #42b983;
+    color: #fff;
+    padding: 10px 15px;
+    border-radius: 5px;
+    border: none;
+    cursor: pointer;
+}
+</style>
