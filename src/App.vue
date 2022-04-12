aedb68d3ac067214493cceafdfb495d5d400d98f
Volodymyr Fihurniak <volodymyr.fihurniak@gmail.com>
2022-04-12 15:45:48 +0300

    Modified the initial project files

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <NavBar />
 </template>
 
+<script>
+import NavBar from '@/components/NavBar'
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,4 +33,5 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import components from '@/components/ui'
+import directives from '@/directives';
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+components.forEach(component => {
+    app.component(component.name, component)
+})
+
+directives.forEach(directive => {
+    app.directive(directive.name, directive)
+})
+
+app.use(Toast, {
+    transition: "Vue-Toastification__bounce",
+    maxToasts: 20,
+    newestOnTop: true
+});
+
+app.use(router).mount('#app')
